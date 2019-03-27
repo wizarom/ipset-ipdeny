868472ae20edb3fc1b2af11d133807ffd759dd82
wizarom <wizarom@gmail.com>
2019-03-27 12:52:42 +0700

    v

--- ipset-setup.sh
+++ ipset-setup.sh
@@ -12,16 +12,20 @@ IPLIST="iplist"
 
 command -v ipset >/dev/null 2>&1 || { echo "I require ipset but it not installed.  Aborting." >&2; exit 1; }
 
-# ipset flush
+# ipset destroy & flush
+ipset destroy
 ipset flush
 
 while read CODE
 do
     ipset create $CODE-set hash:net
+    iptables -F $CODE-net
+    iptables -N $CODE-net
     for IP in $(curl -s http://www.ipdeny.com/ipblocks/data/countries/$CODE.zone)
     do
 	ipset add $CODE-set $IP
     done
-    iptables -I INPUT -m set --match-set $CODE-set src -j DROP
+    iptables -A $CODE-net -m set --match-set $CODE-set src -j DROP
+    iptables -I INPUT -j $CODE-net
 done < $COUNTRYCODE
 exit 0
